7161464048bc8cc6324c38ece3d0c787ef7b7309
Meet Patel <mpatel@plusonex.com>
2024-03-25 12:11:05 +0530

    update routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express"
 import {
+  changeCurrentPassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAcountDetails,
+  updateUserAvtar,
 } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
@@ -28,5 +32,12 @@ router.route("/login").post(loginUser)
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refreshtoken").post(refreshAccessToken)
+router.route("/change-password").post(changeCurrentPasswordrd)
+router.route("/current-user").post(verifyJWT, getCurrentUseretCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAcountDetailsuntDetails)
+router.route("/avtar").patch(verifyJWT, upload.single("avtar"), updateUserAvtar)
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 export default router

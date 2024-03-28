8963f60dca5243dc65b3d5bb1efebce77e938eed
Meet Patel <mpatel@plusonex.com>
2024-03-28 23:40:11 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import {
   changeCurrentPassword,
   getCurrentUser,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
@@ -39,5 +40,8 @@ router.route("/avtar").patch(verifyJWT, upload.single("avtar"), updateUserAvtar)
 router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router

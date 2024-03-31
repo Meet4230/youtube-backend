41abedf694b36324862569a3293723a6b6ba7ce0
Meet Patel <mpatel@plusonex.com>
2024-03-31 18:36:12 +0530

    add additional controllers and routes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -14,6 +14,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    console.log("decodedToken", decodedToken)
 
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
